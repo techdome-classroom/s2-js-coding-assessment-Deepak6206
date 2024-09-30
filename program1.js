ad8f47fe648c05867f50492056550b5097e90072
Deepak6206 <roydeepakdav189@gmail.com>
2024-09-30 14:29:03 +0530

    SOlved

--- program1.js
+++ program1.js
@@ -1,11 +1,26 @@
 /**
- * @param {string} s
+ * @param {string}
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (str) {
+  const stack = [];
+  const pairs = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+
+  for (let char of str) {
+    if (pairs[char]) {
+      stack.push(char);
+    } else if (char === ")" || char === "]" || char === "}") {
+      if (pairs[stack.pop()] !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -2,9 +2,36 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
+function value(r) {
+  if (r == "I") return 1;
+  if (r == "V") return 5;
+  if (r == "X") return 10;
+  if (r == "L") return 50;
+  if (r == "C") return 100;
+  if (r == "D") return 500;
+  if (r == "M") return 1000;
+  return -1;
+}
+var romanToInt = function (str) {
+  let res = 0;
+
+  for (i = 0; i < str.length; i++) {
+    let s1 = value(str.charAt(i));
+
+    if (i + 1 < str.length) {
+      let s2 = value(str.charAt(i + 1));
+
+      if (s1 >= s2) {
+        res = res + s1;
+      } else {
+        res = res + s2 - s1;
+        i++;
+      }
+    } else {
+      res = res + s1;
+    }
+  }
+  return res;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
